071259818e5c4a73cac2cd0d1a30d6884fa8133e
Phattharacharin <your_email@gmail.com>
2026-09-15 15:57:42 +0700

    ปรับheader

--- src/contents/header.js
+++ src/contents/header.js
@@ -3,7 +3,7 @@ import fileName from "../assets/resume_data.pdf"
 export const data = {
     name: "Phattharacharin",
     last: "Khamsitha",
-    title: "B.Sc Senior • Seeking Frontend or Full-stack Internship",
+    title: "B.Sc. Senior • Seeking Frontend or Full-stack Internship",
     period: "Internship Period: Nov 2026 – Mar 2027",
     caption: "Transforming academic concepts into tangible results through hands-on experience.",
     btnText: "Resume",

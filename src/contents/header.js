e309fcb28c0453a87c61ae306f8f93bcfa05633b
Phattharacharin <your_email@gmail.com>
2026-09-19 12:59:45 +0700

    update header

--- src/contents/header.js
+++ src/contents/header.js
@@ -3,7 +3,7 @@ import fileName from "../assets/Portfolio.pdf"
 export const data = {
     name: "Phattharacharin",
     last: "Khamsitha",
-    title: "B.Sc. Senior • Seeking Frontend or Full-stack Internship",
+    title: "B.Sc. Senior • Seeking a Co-op Internship Opportunity in Computer Science",
     period: "Internship Period: Nov 2026 – Mar 2027",
     caption: "Transforming academic concepts into tangible results through hands-on experience.",
     btnText: "Portfolio",

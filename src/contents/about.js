96c50e83bf8b259a8b443c55c7557cfaf6bc01de
Phattharacharin <your_email@gmail.com>
2026-09-19 13:09:29 +0700

    fix Dri

--- src/contents/about.js
+++ src/contents/about.js
@@ -1,4 +1,4 @@
 export const data = {
     title: "About",
-    description: "driven by a passion for computer science and software development, I enjoy building intuitive applications and working with database systems to solve real-world problems. A recent highlight of my work is developing a web application for Suphat Dental Clinic using React, Tailwind CSS, and Supabase. I am eager to secure a Computer Science Co-op Internship to collaborate with experienced teams, tackle new challenges, and contribute value while continuing to expand my technical skills."
+    description: "Driven by a passion for computer science and software development, I enjoy building intuitive applications and working with database systems to solve real-world problems. A recent highlight of my work is developing a web application for Suphat Dental Clinic using React, Tailwind CSS, and Supabase. I am eager to secure a Computer Science Co-op Internship to collaborate with experienced teams, tackle new challenges, and contribute value while continuing to expand my technical skills."
 }
